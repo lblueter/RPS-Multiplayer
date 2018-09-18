687f6db921ec2c8eb10580ab4d11f33e979e50a6
lblueter <logan11023@yahoo.com>
2018-09-18 13:26:57 -0400

    setup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,18 @@ var theyPicked
 
 // var rock = $("#rock").attr("data-type")
 // console.log(rock)
+var config = {
+    apiKey: "AIzaSyDxG4kssZmJXr200W7pU0NOeVdiARpO620",
+    authDomain: "jaguars-21321.firebaseapp.com",
+    databaseURL: "https://jaguars-21321.firebaseio.com",
+    projectId: "jaguars-21321",
+    storageBucket: "jaguars-21321.appspot.com",
+    messagingSenderId: "1052929969797"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
 
 $(document).on("click", "#choice", function () {
     youPicked = $(this).attr("data-type")
